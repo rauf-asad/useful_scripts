141df7ea04fe579358b281f85cd3b2a8dbf60d58
Asad Rauf <asadrauflashari@gmail.com>
2020-02-18 22:57:56 +0500

    Choco

---
+++ chocolatey.bat
@@ -0,0 +1,9 @@
+choco feature enable -n allowGlobalConfirmation
+choco install powerbi
+choco install anaconda3 --params '"/AddToPath /JustMe"'
+choco install 7zip.install
+choco install git.install
+choco install vscode
+choco install sublimetext3
+
+PAUSE
